17a83b30569b96032bd46c5bceb035961965b3e8
christianmasa <christian@linkernetworks.com>
2021-12-24 16:51:39 +0800

    change async socket to sync

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from .event.eventrouter import EventService
 from .dto.eventschema import EventSchema
 from .models.eventmodels import Events
 import os
-import logging
+import socket
 dev = True
 
 if dev:
@@ -96,7 +96,26 @@ async def run_client(ip, port):
         writer.close()
         await writer.wait_closed()
             
-
+async def run_client2(ip, port):
+    
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((ip, port))
+            query = Events.select().where(Events.c.sent == 0)
+            data = await database.fetch_all(query=query)
+            if data:
+                for d in data:
+                    new_data = dict(d.items())
+                    new_data.pop("sent")
+                    id = new_data['id']
+                    new_data.pop("id")
+                    string= ";".join(str(v) for k,v in new_data.items()) +";"
+                    new_data['sent'] = 1
+                    query = Events.update().where(Events.c.id == int(id)).values(**new_data)
+                    sent = False
+                    await database.execute(query=query)
+                    s.send(string.encode("utf-8"))
+            await asyncio.sleep(3)
 
 async def tcp_reconnect():
     host = str(os.environ.get('FE_SOCKET_IP', '0.0.0.0'))
@@ -105,7 +124,7 @@ async def tcp_reconnect():
     while True:
         print('Connecting to server {} ...'.format(server))
         try:
-            await run_client(host, port)
+            await run_client2(host, port)
         except ConnectionRefusedError:
             print('Connection to server {} failed!'.format(server))
         except asyncio.TimeoutError:
